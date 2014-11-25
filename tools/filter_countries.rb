dd8b3346106358609db4e6ef12249a0084fa4708
Ivan Pirlik <abmajor7@gmail.com>
2014-11-25 19:04:35 +0000

    Add filter by hash boundaries

--- tools/filter_countries.rb
+++ tools/filter_countries.rb
@@ -23,24 +23,48 @@ def polygon_within?(polygon, boundary)
   outlyer.nil? ? true : false
 end
 
-Dir.foreach(source_folder) do |country|
+def deep_dup(hash)
+  Marshal.load(Marshal.dump(hash))
+end
 
-  next if country[0].chr == '.'
+def write_dest(dest_folder, iso, hash)
+  File.open(File.join(dest_folder, "#{iso}.geojson"), 'w') do |fw|
+    fw.write(hash.to_json)
+  end
+
+end
+
+Dir.foreach(source_folder) do |file_name|
+
+  next if file_name[0].chr == '.'
+  country = File.basename(file_name, '.geojson')
   puts country
-  hash =  JSON.parse(File.read(File.join(source_folder, country)))
+  hash =  JSON.parse(File.read(File.join(source_folder, file_name)))
   geo = hash['geometry']
   boundary = boundaries[hash['properties']['adm0_a3']]
 
+  targets = {}
   if boundary && geo['type'] == 'MultiPolygon' # only deal with multipolygons for now
-
-    geo['coordinates'].reject! do |polygon|
-      !polygon_within?(polygon, boundary.split(/,s*/))
+    if boundary.is_a?(String)
+      targets[country] = boundary
+    elsif boundary.is_a?(Hash)
+      targets = boundary
     end
+    targets.each do |target, boundary|
+      target_hash = deep_dup(hash)
+      geo = target_hash['geometry']
+      geo['coordinates'].reject! do |polygon|
+        !polygon_within?(polygon, boundary.split(/,s*/))
+      end
+      ['adm0_a3', 'gu_a3', 'su_a3', 'brk_a3', 'iso_a3', 'adm0_a3_is', 'adm0_a3_us'].each do |k|
+        target_hash[k] = target
+      end
+      write_dest(dest_folder, target, target_hash)
+    end
+  else
+    write_dest(dest_folder, country, hash)
   end
 
-  File.open(File.join(dest_folder, country), 'w') do |fw|
-    fw.write(hash.to_json)
-  end
 
 end
 
